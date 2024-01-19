9614d01e969c327e3aa7202933f3fc031a2727ac
yufira <fironovayu@gmail.com>
2024-01-19 11:22:52 +0200

    updated homework 9

--- homework_9.py
+++ homework_9.py
@@ -1,3 +1,6 @@
+import re
+
+
 def input_error(func):
     def inner(*args):
         try:
@@ -13,37 +16,58 @@ def input_error(func):
 
 contacts = {}
 
+
 @input_error
 def hello_handler():
     return 'How can I help you?'
 
+
 @input_error
 def add_handler(command):
-    splitted_command = command.split()
-    name = splitted_command[1]
-    phone = splitted_command[2]
-    contacts[name] = phone
-    return f'Contact {name} with phone {phone} has been added to the contacts'
+    pattern = r'^add [a-zA-Zа-яА-Я]{2,} \d{10,}$'
+    if re.fullmatch(pattern, command):
+        splitted_command = command.split()
+        name = splitted_command[1]
+        phone = splitted_command[2]
+        if name not in contacts.keys() and phone not in contacts.values():
+            contacts[name] = phone
+            return (
+                'Contact {name} with phone {phone} '
+                'has been added to the contacts'
+            ).format(name=name, phone=phone)
+        else:
+            raise ValueError
+    else:
+        return 'Invalid command. Please follow the format: add name phone'
+
 
 @input_error
 def change_handler(command):
-    splitted_command = command.split()
-    name = splitted_command[1]
-    phone = splitted_command[2]
-    if name in contacts:
-        contacts[name] = phone
-        return f'Phone has been changed for {name} to {phone}'
+    pattern = r'^change [a-zA-Zа-яА-Я]{2,} \d{10,}$'
+    if re.fullmatch(pattern, command):
+        splitted_command = command.split()
+        name = splitted_command[1]
+        phone = splitted_command[2]
+        if name in contacts:
+            contacts[name] = phone
+            return f'Phone has been changed for {name} to {phone}'
     else:
         raise IndexError
 
+
 @input_error
 def phone_handler(command):
-    splitted_command = command.split()
-    name = splitted_command[1]
-    if name in contacts:
-        return f'The phone number for {name} is {contacts[name]}'
+    pattern = r'^phone [a-zA-Zа-яА-Я]{2,}$'
+    if re.fullmatch(pattern, command):
+        splitted_command = command.split()
+        name = splitted_command[1]
+        if name in contacts:
+            return f'The phone number for {name} is {contacts[name]}'
+        else:
+            raise IndexError
     else:
-        raise IndexError
+        raise ValueError
+
 
 @input_error
 def show_all_handler():
@@ -60,26 +84,37 @@ def show_all_handler():
 def exit_handler():
     return 'Good bye!'
 
+
 @input_error
 def main():
+    handlers_with_command = {
+        'add': add_handler,
+        'change': change_handler,
+        'phone': phone_handler,
+    }
+
+    handlers_without_command = {
+        'hello': hello_handler,
+        'show all': show_all_handler,
+        'good bye': exit_handler,
+        'close': exit_handler,
+        'exit': exit_handler
+    }
+
     while True:
         command = input('Please enter command: ').lower()
 
-        if command == 'hello':
-            print(hello_handler())
-        elif command.startswith('add'):
-            print(add_handler(command))
-        elif command.startswith('change'):
-            print(change_handler(command))
-        elif command.startswith('phone'):
-            print(phone_handler(command))
-        elif command == 'show all':
-            print(show_all_handler())
-        elif command in ['good bye', 'close', 'exit']:
-            print(exit_handler())
+        if command == '.' or command in ['good bye', 'close', 'exit']:
             break
-        elif command == '.':
-            break
+
+        for prefix, handler in handlers_with_command.items():
+            if command.startswith(prefix):
+                print(handler(command))
+                break
+        for prefix, handler in handlers_without_command.items():
+            if command.startswith(prefix):
+                print(handler())
+                break
 
 
 if __name__ == "__main__":
